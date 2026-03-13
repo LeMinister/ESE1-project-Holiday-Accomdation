b73cbab0cf1ec243cdefcae106805971d5aad903
LeMinister <ulrich_melvin@yahoo.co.uk>
2026-03-13 10:37:32 +0000

    backend minus test

--- Backend/api/admin.py
+++ Backend/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Property, Booking
 
-# Register your models here.
+
+@admin.register(Property)
+class PropertyAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "price_per_night")
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ("booking_reference", "property", "guest_name", "check_in", "check_out")
