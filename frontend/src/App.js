c9de6c97270e8d9b53b1381bf77d1278099cae86
LeMinister <ulrich_melvin@yahoo.co.uk>
2026-03-13 12:10:02 +0000

    implement theme

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,14 @@
-import React from "react";
-
-import Login from "./pages/Login";
-import Register from "./pages/Register";
-import Properties from "./pages/Properties";
-import ManageBooking from "./pages/ManageBooking";
+// Frontend/src/App.js
+import React from 'react';
+import './App.css';
 
 function App() {
-
   return (
-    <div>
-
-      <h1>Holiday Booking System</h1>
-
-      <Properties />
-
-      <Login />
-
-      <Register />
-
-      <ManageBooking />
-
+    <div className="App">
+      <header className="App-header">
+        <h1>Holiday Accommodation Booking System</h1>
+        <p>Welcome! Your backend API is running and ready.</p>
+      </header>
     </div>
   );
 }
